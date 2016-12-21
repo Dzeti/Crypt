10953a9efd487346bee28a6042590e3c40cdc53c
Dzeti <tania083@mail.ru>
2016-12-21 20:28:34 +0300

    Add files via upload

---
+++ Crypto/Crypto/main.Designer.cs
@@ -0,0 +1,413 @@
+﻿namespace Crypto
+{
+    partial class main
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TacControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.TacControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TacControl1
+            // 
+            this.TacControl1.Controls.Add(this.tabPage1);
+            this.TacControl1.Controls.Add(this.tabPage2);
+            this.TacControl1.Controls.Add(this.tabPage3);
+            this.TacControl1.Controls.Add(this.tabPage4);
+            this.TacControl1.Location = new System.Drawing.Point(0, 12);
+            this.TacControl1.Name = "TacControl1";
+            this.TacControl1.SelectedIndex = 0;
+            this.TacControl1.Size = new System.Drawing.Size(540, 299);
+            this.TacControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(532, 273);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DES";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Расшифрованное сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Зашифрованное сообщение";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите сообщение";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(500, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(500, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(501, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox6);
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(532, 273);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "RSA";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Расшифрованное сообщение";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Зашифрованное сообщение";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(16, 190);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(484, 20);
+            this.textBox6.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(16, 141);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(484, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Введите сообщение";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 37);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(487, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.textBox8);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.textBox7);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(532, 273);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "SHA-2";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.textBox11);
+            this.tabPage4.Controls.Add(this.button5);
+            this.tabPage4.Controls.Add(this.textBox10);
+            this.tabPage4.Controls.Add(this.button4);
+            this.tabPage4.Controls.Add(this.label9);
+            this.tabPage4.Controls.Add(this.textBox9);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(532, 273);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "ЭЦП";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(11, 34);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(468, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Введите сообщение";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Найти хеш";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(11, 156);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(468, 20);
+            this.textBox8.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Хеш";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(10, 35);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(387, 20);
+            this.textBox9.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 19);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Введите сообщение";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 46);
+            this.button4.TabIndex = 2;
+            this.button4.Tag = "";
+            this.button4.Text = "Подписать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(10, 146);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(163, 20);
+            this.textBox10.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(225, 80);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(172, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Проверить подпись";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(225, 146);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(172, 20);
+            this.textBox11.TabIndex = 5;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 311);
+            this.Controls.Add(this.TacControl1);
+            this.Name = "main";
+            this.Text = "Лаб. №5";
+            this.TacControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TacControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button5;
+
+    }
+}
+
---
+++ Crypto/Crypto/main.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Crypto
+{
+    public partial class main : Form
+    {
+        string strFin, s, encrypt1;
+        public main()
+        {
+            InitializeComponent();
+            textBox10.BackColor = Color.Red;
+            textBox11.BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string encrypt;
+            string source = Convert.ToString(textBox1.Text);
+            Library.DESLib crypt = new Library.DESLib();
+            Stream sourceData = new MemoryStream(Encoding.Default.GetBytes(source));
+            Stream encryptedData = new MemoryStream();
+            crypt.EncryptData(sourceData, encryptedData);
+            Stream decryptedData = new MemoryStream();
+            crypt.DecryptData(encryptedData, decryptedData);
+            byte[] bufRead2 = new byte[encryptedData.Length];
+            encryptedData.Position = 0;
+            encryptedData.Read(bufRead2, 0, Convert.ToInt32(encryptedData.Length));
+            encrypt = Convert.ToBase64String(bufRead2);
+            textBox2.Text = encrypt;
+            byte[] bufRead1 = new byte[encryptedData.Length];
+            decryptedData.Position = 0;
+            decryptedData.Read(bufRead1, 0, Convert.ToInt32(decryptedData.Length));
+            string decrypt = Encoding.Default.GetString(bufRead1);
+            textBox3.Text = decrypt;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            RSA_Lib.RSA crypt = new RSA_Lib.RSA();
+            string encrypt;
+            string source = Convert.ToString(textBox4.Text);
+
+            Stream sourceData = new MemoryStream(Encoding.Default.GetBytes(source));
+            Stream encryptedData = new MemoryStream();
+            string s = crypt.RSALib(sourceData, encryptedData);
+            textBox5.Text = s;
+            byte[] bufRead2 = new byte[encryptedData.Length];
+            encryptedData.Position = 0;
+            encryptedData.Read(bufRead2, 0, Convert.ToInt32(encryptedData.Length));
+            encrypt = System.Text.Encoding.Default.GetString(bufRead2);
+            textBox6.Text = encrypt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Sha2.Sha2 sha = new Sha2.Sha2();
+            string sourceDataString = textBox7.Text;
+            string strFin1;
+            MemoryStream sourceDataStream = new MemoryStream(Encoding.Default.GetBytes(sourceDataString));
+            MemoryStream HashStream = new MemoryStream();
+            sha.HashFile(sourceDataStream, HashStream);
+            byte[] a = new byte[32];
+            HashStream.Position = 0;
+            HashStream.Read(a, 0, a.Length);
+            strFin1 = Sha2.Util.ArrayToString(a);
+            textBox8.Text = strFin1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Sha2.Sha2 sha = new Sha2.Sha2();
+            string sourceDataString = textBox9.Text;
+            MemoryStream sourceDataStream = new MemoryStream(Encoding.Default.GetBytes(sourceDataString));
+            MemoryStream HashStream = new MemoryStream();
+            sha.HashFile(sourceDataStream, HashStream);
+            byte[] a = new byte[32];
+            HashStream.Position = 0;
+            HashStream.Read(a, 0, a.Length);
+            strFin = Sha2.Util.ArrayToString(a);
+            RSA_Lib.RSA crypt = new RSA_Lib.RSA();
+            Stream sourceData = new MemoryStream(Encoding.Default.GetBytes(strFin));
+            Stream encryptedData = new MemoryStream();
+            s = crypt.RSALib(sourceData, encryptedData);
+            byte[] bufRead2 = new byte[encryptedData.Length];
+            encryptedData.Position = 0;
+            encryptedData.Read(bufRead2, 0, Convert.ToInt32(encryptedData.Length));
+            encrypt1 = System.Text.Encoding.Default.GetString(bufRead2);
+            textBox10.Text = "Сообщение подписано";
+            textBox10.BackColor = Color.Green;
+
+        
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (strFin == encrypt1)
+            {
+                textBox11.Text = "Подпись верна";
+                textBox11.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox11.Text = "Подпись неверна";
+              
+            }
+
+        }
+    }
+}
