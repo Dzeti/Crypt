3efe5a6ebbfb1f5a328f2e2cb78317218ddbd15d
Dzeti <tania083@mail.ru>
2016-12-23 16:39:03 +0300

    Add files via upload

---
+++ Sha2 lab.3/Sha2/Util.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Sha2
+{
+    public static class Util
+    {
+        public static string ArrayToString(byte[] arr)
+        {
+            StringBuilder s = new StringBuilder(arr.Length * 2);
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                s.AppendFormat("{0:x2}", arr[i]);
+            }
+
+            return s.ToString();
+        }
+    }
+}
---
+++ Sha2 lab.3/ShaTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+
+namespace ShaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConstAndHashAreEqual()
+        {
+            Sha2.Sha2 sha = new Sha2.Sha2();
+            string sourceDataString = "АБВГ";
+            string strFin, consta = "72d2a70d03005439de209bbe9ffc050fafd891082e9f3150f05a61054d25990f";
+            MemoryStream sourceDataStream = new MemoryStream(Encoding.Default.GetBytes(sourceDataString));
+            MemoryStream HashStream = new MemoryStream();
+            sha.HashFile(sourceDataStream, HashStream);
+            byte[] a = new byte[32];
+            HashStream.Position = 0;
+            HashStream.Read(a, 0, a.Length);
+            strFin = Sha2.Util.ArrayToString(a);
+            Assert.AreEqual(consta, strFin);
+
+        }
+    }
+}
